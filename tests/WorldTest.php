c0294415ab8fe5351322a7d6d505a04a1bc3fe6c
Larry Garfield <larry@garfieldtech.com>
2017-03-04 17:40:19 -0600

    Add multiplayer test.

--- tests/WorldTest.php
+++ tests/WorldTest.php
@@ -119,4 +119,24 @@ class WorldTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('1', (string)$w->cellAt(1, 3));
     }
 
+    public function testStepMultiplayer()
+    {
+        $w = new World(5, 10);
+
+        $w->place('1', 2, 2)
+            ->place('2', 2, 3)
+            ->place('1', 2, 4);
+
+        $w->step();
+
+        // Because they're different species, everyone should have died.
+        $this->assertEquals('E', (string)$w->cellAt(2, 2));
+        $this->assertEquals('E', (string)$w->cellAt(2, 3));
+        $this->assertEquals('E', (string)$w->cellAt(2, 4));
+
+        // No one should be born.
+        $this->assertEquals('E', (string)$w->cellAt(1, 3));
+        $this->assertEquals('E', (string)$w->cellAt(3, 3));
+    }
+
 }
